435111d2dea4b7a695ca1f3f33409c06224f3a64
Ari Effendi <691465+arifnd@users.noreply.github.com>
2024-12-10 11:14:39 +0700

    First commit

---
+++ laravel/xdebug/dev.nix
@@ -0,0 +1,48 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.php82
+    (pkgs.php82.buildEnv {
+      extensions = ({ enabled, all }: enabled ++ (with all; [
+        xdebug
+      ]));
+      extraConfig = ''
+        xdebug.mode=debug,coverage
+      '';
+    })
+    pkgs.php82Packages.composer
+    pkgs.nodejs_20
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "README.md" "resources/views/welcome.blade.php" ];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "php" "artisan" "serve" "--port" "$PORT" "--host" "0.0.0.0" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
